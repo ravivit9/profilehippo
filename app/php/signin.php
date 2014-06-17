4589b143d8ce2a21b7c909a1ff542e260fe653cd
ravivit9 <ravindra.bharathi@outlook.com>
2014-06-17 22:11:39 +0100

    Login Password Hashing Code
    Login Password Hashing code

--- app/php/signin.php
+++ app/php/signin.php
@@ -17,8 +17,13 @@ $pwd = mysql_real_escape_string($data->pwd);
 $pwd = mysql_real_escape_string($data->pwd);
 $uemail = mysql_real_escape_string($data->email);
 
+#$salt = bin2hex(mcrypt_create_iv(32, MCRYPT_DEV_URANDOM));
+#$saltedPW =  $escapedPW . $salt;
+#$saltedPW =  $pwd . $salt ;
+#$hashedPW = hash('sha256', $saltedPW);
+$hashedPW = hash('sha256', $pwd);
  
-$qry_em = 'select count(*) as cnt from members where mem_email_id ="' . $uemail . '"' . ' and mem_password="' . $pwd . '"' ;
+$qry_em = 'select count(*) as cnt from members where mem_email_id ="' . $uemail . '"' . ' and mem_password="' . $hashedPW . '"' ;
 $qry_res = mysql_query($qry_em);
 $res = mysql_fetch_assoc($qry_res);
  
@@ -28,7 +33,7 @@ $res = mysql_fetch_assoc($qry_res);
 #$hashedPW = hash('sha256', $saltedPW);
  
 if ($res['cnt'] == 0) {
-    $arr = array('msg' => "", 'error' => 'Invalid user or password. Please check the email id / password.');
+    $arr = array('msg' => "", 'error' => 'Invalid user or password. Please check the email id / password.' . $hashedPW);
     $jsn = json_encode($arr);
     print_r($jsn);
 }else {
--- app/php/signup.php
+++ app/php/signup.php
@@ -10,7 +10,7 @@
 #mysql_select_db('devschema', $con) or die('');
  
 #000.186.239.3:3306 -- GoDaddy host 
-mysql_connect("000.186.239.3:3306","profilehippo","\$sysDate1") or die(mysql_error());
+mysql_connect("localhost","profilehippo","\$sysDate1") or die(mysql_error());
 mysql_select_db("devschema") or die(mysql_error());
 
 $data = mysql_query("SELECT * FROM members") or die(mysql_error());
@@ -41,11 +41,12 @@ $res = mysql_fetch_assoc($qry_res);
 # generate a random salt to use for this account
 #$salt = bin2hex(mcrypt_create_iv(32, MCRYPT_DEV_URANDOM));
 #$saltedPW =  $escapedPW . $salt;
-#$hashedPW = hash('sha256', $saltedPW);
+#$saltedPW =  $pwd . $salt;
+$hashedPW = hash('sha256', $pwd);
  
 if ($res['cnt'] == 0) {
     #$qry = 'INSERT INTO login_master_t (lm_fname, lm_login_pwd, lm_login_email) values ("' . $usrname . '","' . $upswd . '","' . $uemail . '")';
-    $qry = 'INSERT INTO members (mem_first_name, mem_last_name, mem_password, mem_email_id)values ("' . $usrfname . '","' . $usrlname . '","' . $pwd . '","' . $uemail . '")';
+    $qry = 'INSERT INTO members (mem_first_name, mem_last_name, mem_password, mem_email_id)values ("' . $usrfname . '","' . $usrlname . '","' . $hashedPW . '","' . $uemail . '")';
     $qry_res = mysql_query($qry);
     if ($qry_res) {
         $arr = array('msg' => "User Created Successfully!!!", 'Success' => '','redirectto' => '/signin');
