024b1c02c8bc6d90e91b5224eaf4051fe5a881c2
ravivit9 <ravindra.bharathi@outlook.com>
2014-06-27 19:45:22 +0100

    pwd change
    pwd change

--- app/php/config.php
+++ app/php/config.php
@@ -4,7 +4,7 @@ if ($env == 'DEVELOPMENT') {
     mysql_connect("localhost","profilehippo","\$sysDate1") or die(mysql_error());
     mysql_select_db("devschema") or die(mysql_error());
 }else {
-    mysql_connect("www.rbkconsultancy.co.uk","profilehippo","sysDate1") or die(mysql_error());
+    mysql_connect("www.rbkconsultancy.co.uk","profilehippo","\$sysDate1") or die(mysql_error());
     mysql_select_db("devschema") or die(mysql_error());
 }
 ?>
