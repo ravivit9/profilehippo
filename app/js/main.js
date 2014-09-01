4d306f86a9f7fa61c4b3631a69a5ce04213462c2
ravivit9 <ravindra.bharathi@outlook.com>
2014-09-01 22:42:54 +0100

    Added Angular (Animate, Messages, Mocks, motion, Strap)
    Added Angular (Animate, Messages, Mocks, motion, Strap) and update other
    dependencies libraries.

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,6 @@
 require.config({
 	paths: {
-		angular: '../../bower_components/angular/angular',
+		angular: '../../bower_components/angular/angular.min',
         lodash: '../../bower_components/lodash/dist/lodash.min',
 		//angularRoute: '../../bower_components/angular-route/angular-route',
 		uiRouter: '../../bower_components/angular-ui-router/release/angular-ui-router.min',
@@ -9,6 +9,11 @@ require.config({
         d3:'../../bower_components/d3/d3.min',
         angularD3Charts:'../../bower_components/angular-charts/dist/angular-charts.min',
 		angularMocks: '../../bower_components/angular-mocks/angular-mocks',
+        angularMessages: '../../bower_components/angular-messages/angular-messages.min',
+        angularAnimate: '../../bower_components/angular-animate/angular-animate.min',
+        angularStrap: '../../bower_components/angular-strap/dist/angular-strap.min',
+        angularStrapTpl: '../../bower_components/angular-strap/dist/angular-strap.tpl.min',
+        satellizer: '../../bower_components//satellizer/satellizer.min',
         bootstrap: '../../bower_components/bootstrap/dist/js/bootstrap.min',
         jQuery: '../../bower_components/jquery/dist/jquery.min',
         restangular: '../../bower_components/restangular/dist/restangular.min'
@@ -23,7 +28,12 @@ require.config({
         'angularData': ['angular'],
         'angularCache': ['angular'],
         'angularD3Charts': ['d3', 'angular'],
-		'angularMocks': {
+		'angularMessages': ['angular'],
+        'angularAnimate':['angular'],
+        'angularStrap':['angular'],
+        'angularStrapTpl':['angular','angularStrap'],
+        'satellizer':['angular'],
+        'angularMocks': {
 			deps:['angular'],
 			'exports':'angular.mock'
 		},
@@ -50,8 +60,13 @@ require( [
     'angularData',
     'd3',
     'angularD3Charts',
+    'angularMessages',
+    'angularAnimate',
+    'angularStrap',
+    'angularStrapTpl',
+    'satellizer',
     'bootstrap'
-], function(angular, app, routes) {
+], function(angular, app, routes,angulardata, d3, angulard3charts, angularmessages, angularanimate, angularstrap, angularstraptpl,satellizer) {
 	'use strict';
 	var $html = angular.element(document.getElementsByTagName('html')[0]);
 
--- app/js/services.js
+++ app/js/services.js
@@ -71,6 +71,32 @@ define(['angular'], function (angular) {
               return $q.reject(response);
             }
           };
+
+        /*
+		// More info on $q: docs.angularjs.org/api/ng.$q Of course it's possible to define more dependencies. 
+		return function(promise) {
+			//The promise is not resolved until the code defined in the interceptor has not finished its execution
+			return promise.then(function(response) {
+
+				// response.status >= 200 && response.status <= 299 The http request was completed successfully. Before to resolve the promise I can do whatever I want! For example: add a new property to the promise returned from the server. 
+				response.data.extra = 'Interceptor strikes back';
+				// ... or even something smarter. Return the execution control to the code that initiated the request.
+				return response; 
+			}, function(response) {
+				// The HTTP request was not successful. It's possible to use interceptors to handle specific errors. For example:
+				if (response.status === 401) {
+					// HTTP 401 Error: The request requires user authentication 
+					response.data = { 
+				 		status: false, 
+				 		description: 'Authentication required!'
+				 	};
+					return response;
+				}
+				// $q.reject creates a promise that is resolved as rejectedwith the specified reason. In this case the error callback will be executed.
+				return $q.reject(response);
+			});
+		}*/
+        
         })
         .directive('loginDialog', function (AUTH_EVENTS) {
           return {
