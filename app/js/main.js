15766e900541dda5f330e5ccffbfbbfc6c9c479f
ravivit9 <ravindra.bharathi@outlook.com>
2014-09-06 16:11:45 +0100

    Library Clean up
    Library Clean up.

--- app/js/main.js
+++ app/js/main.js
@@ -4,20 +4,13 @@ require.config({
         lodash: '../../bower_components/lodash/dist/lodash.min',
 		//angularRoute: '../../bower_components/angular-route/angular-route',
 		uiRouter: '../../bower_components/angular-ui-router/release/angular-ui-router.min',
-        angularData:'../../bower_components/angular-data/dist/angular-data.min',
-        angularCache:'../../bower_components/angular-cache/dist/angular-cache.min',
-        d3:'../../bower_components/d3/d3.min',
-        angularD3Charts:'../../bower_components/angular-charts/dist/angular-charts.min',
 		angularMocks: '../../bower_components/angular-mocks/angular-mocks',
         angularMessages: '../../bower_components/angular-messages/angular-messages.min',
-        angularAnimate: '../../bower_components/angular-animate/angular-animate.min',
-        angularStrap: '../../bower_components/angular-strap/dist/angular-strap.min',
-        angularStrapTpl: '../../bower_components/angular-strap/dist/angular-strap.tpl.min',
-        satellizer: '../../bower_components//satellizer/satellizer.min',
         bootstrap: '../../bower_components/bootstrap/dist/js/bootstrap.min',
         jQuery: '../../bower_components/jquery/dist/jquery.min',
-        restangular: '../../bower_components/restangular/dist/restangular.min'
-	},
+        restangular: '../../bower_components/restangular/dist/restangular.min',
+	    domReady:'../../bower_components/requirejs-domready/domReady'
+    },
 	baseUrl: 'app/js',
 	shim: {
 		'angular' : {exports : 'angular'},
@@ -25,14 +18,7 @@ require.config({
         'uiRouter':{
             deps: ['angular']
         },
-        'angularData': ['angular'],
-        'angularCache': ['angular'],
-        'angularD3Charts': ['d3', 'angular'],
 		'angularMessages': ['angular'],
-        'angularAnimate':['angular'],
-        'angularStrap':['angular'],
-        'angularStrapTpl':['angular','angularStrap'],
-        'satellizer':['angular'],
         'angularMocks': {
 			deps:['angular'],
 			'exports':'angular.mock'
@@ -57,16 +43,9 @@ require( [
 	'angular',
 	'app',
 	'ui-routes',
-    'angularData',
-    'd3',
-    'angularD3Charts',
     'angularMessages',
-    'angularAnimate',
-    'angularStrap',
-    'angularStrapTpl',
-    'satellizer',
     'bootstrap'
-], function(angular, app, routes,angulardata, d3, angulard3charts, angularmessages, angularanimate, angularstrap, angularstraptpl,satellizer) {
+], function(angular, app, routes, angularmessages, bootstrap) {
 	'use strict';
 	var $html = angular.element(document.getElementsByTagName('html')[0]);
 
